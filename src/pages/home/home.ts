a25f5b09255eb8a44cbe73a2de374ffc2c0c8d22
Alkahtani <alkahtan001@gannon.edu>
2016-11-02 14:34:08 -0400

    Upload ListApp

---
+++ src/pages/home/home.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { NavController } from 'ionic-angular';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+items
+
+  constructor(public navCtrl: NavController, private http: Http) {
+
+    this.http.get("http://localhost:3000/db").map(res => res.json()).subscribe(data=> {
+      this.items = data;
+      console.log("GOT RESULTS");
+    },
+    error => {
+      console.log("OPPS!");
+    });
+
+  }
+
+}
